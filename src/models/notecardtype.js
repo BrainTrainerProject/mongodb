228776a6452f987a75b8177b627a9921a9196a95
Unknown <christian_philipp.sassmann@fh-bielefeld.de>
2017-05-30 08:49:47 +0200

    case sensitive heroku add

---
+++ src/models/notecardtype.js
@@ -0,0 +1,78 @@
+import mongoose from 'mongoose';
+import mongodb from 'mongodb';
+import mapper from './mapper';
+
+const ObjectId = mongodb.ObjectID;
+
+const notecardTypeSchema = mongoose.Schema({
+  name: String,
+});
+
+const NotecardType = mongoose.model('Notecardtype', notecardTypeSchema);
+
+/*
+READ ONE
+Kommentar
+*/
+exports.findById = (id, callback) => {
+  const isValidObjectId = (id.match(/^[0-9a-fA-F]{24}$/));
+  if (!isValidObjectId) {
+    return callback(new Error('Invalid ObjectId', 400), null);
+  }
+  NotecardType.findOne({ id: ObjectId(id) }, (err, type) => {
+    if (!err && type) {
+      callback(err, mapper.convertNotecardTypeToJsonResponse(type));
+    } else {
+      callback(err, null);
+    }
+  });
+  return null;
+};
+
+/*
+READ ALL
+*/
+exports.findAll = (callback) => {
+  NotecardType.find({}, (err, types) => {
+    const notecardTypeMap = {};
+    for (let i = 0; i < types.length; i += 1) {
+      notecardTypeMap[types[i].id] = mapper.convertNotecardTypeToJsonResponse(types[i]);
+    }
+    callback(err, notecardTypeMap);
+  });
+};
+
+/*
+CREATE
+*/
+exports.createNotecardType = (json, callback) => {
+  const newCardType = new NotecardType({
+    name: json.name,
+  });
+  newCardType.save((err) => {
+    callback(err);
+  });
+};
+
+/*
+DELETE
+*/
+exports.deleteNotecardType = (id, callback) => {
+  NotecardType.findOneAndRemove({ _id: ObjectId(id) },
+    (err, result) => {
+      if (err) return (callback(new Error('Delete NotecardType', 500), result));
+      return result;
+    });
+};
+
+/*
+UPDATE
+*/
+exports.updateNotecardType = (id, json, callback) => {
+  NotecardType.findOneAndUpdate({ _id: ObjectId(id) }, {
+    $set: json,
+  }, (err, result) => {
+    if (err) return (callback(new Error('Update NotecardType', 400), result));
+    return result;
+  });
+};
---
+++ src/models/profile.js
@@ -0,0 +1,87 @@
+import mongoose from 'mongoose';
+import mongodb from 'mongodb';
+import mapper from './mapper';
+
+const ObjectId = mongodb.ObjectID;
+
+const profileSchema = mongoose.Schema({
+  email: String,
+  oauthtoken: String,
+  photourl: String,
+  follower: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Profile' }],
+  visibility: Boolean,
+  sets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Set' }],
+});
+
+const Profile = mongoose.model('Profile', profileSchema);
+
+/*
+READ ONE
+*/
+exports.findById = (id, callback) => {
+  const isValidObjectId = (id.match(/^[0-9a-fA-F]{24}$/));
+  if (!isValidObjectId) {
+    return callback(new Error('Invalid ObjectId', 400), null);
+  }
+  Profile.findOne({ id: ObjectId(id) }, (err, profile) => {
+    if (!err && profile) {
+      callback(err, mapper.convertProfileToJsonResponse(profile));
+    } else {
+      callback(err, null);
+    }
+  });
+  return null;
+};
+
+/*
+READ ALL
+*/
+exports.findAll = (callback) => {
+  Profile.find({}, (err, profiles) => {
+    const profileMap = {};
+    for (let i = 0; i < profiles.length; i += 1) {
+      profileMap[profiles[i].id] = mapper.convertProfileToJsonResponse(profiles[i]);
+    }
+    callback(err, profileMap);
+  });
+};
+
+/*
+CREATE
+*/
+exports.createProfile = (json, callback) => {
+  const newProfile = new Profile({
+    email: json.email,
+    oauthtoken: json.oauthtoken,
+    photourl: json.photourl,
+    follower: json.follower,
+    visibility: json.visibility,
+    sets: json.sets,
+  });
+  newProfile.save((err) => {
+    callback(err);
+  });
+};
+
+/*
+DELETE
+*/
+exports.deleteProfile = (id, callback) => {
+  Profile.findOneAndRemove({ _id: ObjectId(id) },
+    (err, result) => {
+      if (err) return (callback(new Error('Delete Profile', 500), result));
+      return result;
+    });
+};
+
+/*
+UPDATE
+*/
+exports.updateProfile = (id, json, callback) => {
+  Profile.findOneAndUpdate({ _id: ObjectId(id) }, {
+    $set: json,
+  }, (err, result) => {
+    if (err) return (callback(new Error('Update Profile', 400), result));
+    return result;
+  });
+};
